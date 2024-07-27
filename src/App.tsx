6a20b8262430a7f23feb6b1e3c21172ae9f766d2
lapushis <lapkras@gmail.com>
2024-07-27 19:02:18 +0500

    added header + added lorem on main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,62 @@
 import Avatar from 'components/Avatar'
+import Header from 'components/Header'
 
 export default function () {
   return (
-    <div className="container mx-auto prose max-w-prose p-10">
-      <h1>
-        Hi! <br />
-        I'm Lapushis, and this is my website WIP
-      </h1>
-      <div className="divider divider-neutral" />
-      <Avatar />
-    </div>
+    <>
+      <Header />
+      <div className="container max-w-screen-lg mx-auto p-4 bg-base-200">
+        <h1 className="text-4xl font-bold">
+          Hi! <br />
+          I'm Lapushis, and this is my website WIP
+        </h1>
+        <div className="divider divider-neutral" />
+        <p className="prose text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          consequuntur culpa explicabo sed saepe soluta voluptates magni, non
+          dolorem quas velit omnis incidunt laboriosam quaerat necessitatibus!
+          Est, eveniet. Doloremque, qui?
+        </p>
+
+        <p className="prose text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          consequuntur culpa explicabo sed saepe soluta voluptates magni, non
+          dolorem quas velit omnis incidunt laboriosam quaerat necessitatibus!
+          Est, eveniet. Doloremque, qui?
+        </p>
+        <p className="prose text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          consequuntur culpa explicabo sed saepe soluta voluptates magni, non
+          dolorem quas velit omnis incidunt laboriosam quaerat necessitatibus!
+          Est, eveniet. Doloremque, qui?
+        </p>
+        <p className="prose text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          consequuntur culpa explicabo sed saepe soluta voluptates magni, non
+          dolorem quas velit omnis incidunt laboriosam quaerat necessitatibus!
+          Est, eveniet. Doloremque, qui?
+        </p>
+        <p className="prose text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          consequuntur culpa explicabo sed saepe soluta voluptates magni, non
+          dolorem quas velit omnis incidunt laboriosam quaerat necessitatibus!
+          Est, eveniet. Doloremque, qui?
+        </p>
+        <p className="prose text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          consequuntur culpa explicabo sed saepe soluta voluptates magni, non
+          dolorem quas velit omnis incidunt laboriosam quaerat necessitatibus!
+          Est, eveniet. Doloremque, qui?
+        </p>
+        <p className="prose text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          consequuntur culpa explicabo sed saepe soluta voluptates magni, non
+          dolorem quas velit omnis incidunt laboriosam quaerat necessitatibus!
+          Est, eveniet. Doloremque, qui?
+        </p>
+
+        <Avatar />
+      </div>
+    </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,108 @@
+const Logo = () => {
+  return (
+    <h3 className="text-xl font-bold">
+      <a className="uppercase text-3xl" href="/">
+        Lapushis
+      </a>
+    </h3>
+  )
+}
+
+const NavLinksContent = () => {
+  return (
+    <>
+      <li>
+        <a class=" hover:text-primary">Gallery</a>
+      </li>
+      <li>
+        <a class=" hover:text-primary">Commissions</a>
+      </li>
+      <li>
+        <a class=" hover:text-primary">Terms of service</a>
+      </li>
+    </>
+  )
+}
+
+const FIXED_LINKS_CONTENT = [
+  {
+    name: 'Kofi',
+    link: 'https://ko-fi.com/lapushis',
+    img: '/kofi.png',
+  },
+  // {
+  //   name: 'Patreon',
+  //   link: 'https://patreon.com/lapushis',
+  //   img: '/patreon.png',
+  // },
+]
+
+const FixedLink = ({
+  name,
+  link,
+  img,
+}: {
+  name: string
+  link: string
+  img: string
+}) => {
+  return (
+    <a class="flex items-center hover:text-primary" href={link} target="_blank">
+      <img className="h-12" src={img} />
+      <span className="max-lg:hidden text-xl font-bold ml-2">{name}</span>
+    </a>
+  )
+}
+
+const Header = () => {
+  return (
+    <header className="bg-base-100 sticky top-0">
+      <div className="flex bg-transparent container max-w-screen-lg mx-auto p-4  items-center justify-between">
+        <div className="flex items-center">
+          <div className="dropdown block lg:none">
+            <div tabindex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <nav>
+              <ul
+                tabindex={0}
+                className="menu menu-lg dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                <NavLinksContent />
+              </ul>
+            </nav>
+          </div>
+          <Logo />
+        </div>
+
+        <nav className="flex justify-end items-center">
+          <div className="hidden lg:flex">
+            <ul className="px-2 flex justify-between items-center gap-3 text-xl">
+              <NavLinksContent />
+            </ul>
+          </div>
+          <div className="flex gap-4 ml-4">
+            {FIXED_LINKS_CONTENT.map(({ name, img, link }) => (
+              <FixedLink key={link} name={name} link={link} img={img} />
+            ))}
+          </div>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
