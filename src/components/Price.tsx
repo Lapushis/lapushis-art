addd33fec19f71473fa35bb83cc7c3095d8da05d
lapushis <lapkras@gmail.com>
2024-08-11 20:47:37 +0500

    added proper price cards

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,7 +1,108 @@
+interface ICommissionCardProps {
+  title: string
+  body: string
+  price: string
+  images: string[]
+  cardPosition: number
+}
+
+const CommissionTypeCard = ({
+  title,
+  body,
+  price,
+  images,
+  cardPosition,
+}: ICommissionCardProps) => {
+  return (
+    <div className="card bg-base-200 shadow-xl mt-8">
+      <div className="card-body flex flex-col md:flex-row justify-between">
+        <div className="prose max-w-prose">
+          <h2 className="card-title text-3xl">{title}</h2>
+          <p className="text-lg">{body}</p>
+        </div>
+        <div className="divider md:divider-horizontal justify-self-end" />
+        <div>
+          <h3 className="text-2xl md:text-4xl font-bold justify-self-end">
+            {price}
+          </h3>
+        </div>
+      </div>
+
+      <figure className="relative bg-secondary">
+        <div className="rounded-box flex flex-col gap-4 min-[750px]:flex-row  min-[750px]:max-h-[450px]">
+          {images.map((src, index) => (
+            <div
+              key={`image-${index}`}
+              class="rounded-box flex-shrink flex-grow-0 basis-auto max-w-fit"
+            >
+              <img src={src} class="h-full w-full object-cover" />
+            </div>
+          ))}
+        </div>
+      </figure>
+    </div>
+  )
+}
+
+const COMMISSION_INFO = [
+  {
+    title: 'Footshot',
+    body: "Full rendered artwork centered around character's foot/feet. 80$ per 1 picture flat. Max 2 characters per picture. Alt versions with dirt, debris, crushed buildings / micros, etc. are included in the price, as well as simple background.",
+    price: '80$',
+    images: [
+      '/price/footshot1.png',
+      '/price/footshot2.png',
+      '/price/footshot3.png',
+    ],
+  },
+  {
+    title: 'Halfbody',
+    body: `Full rendered artwork.
+120-150$ per 1 character depending on design complexity, background included.
+Very complex background +30$.
+Each additional character +80-110$, no more than 2 additional characters. Micro random characters don't count.
+`,
+    price: '$120-150',
+    images: [
+      '/price/halfbody1.png',
+      '/price/halfbody2.png',
+      '/price/halfbody3.png',
+    ],
+  },
+  {
+    title: 'Fullbody',
+    body: `Full rendered artwork.
+160-190$ per 1 character depending on design complexity, background included.
+Very complex background +30$.
+Each additional character +140-160$, no more than 2 additional characters. Micro random characters don't count.
+`,
+    price: '$160-190',
+    images: [
+      '/price/fullbody1.png',
+      '/price/fullbody2.png',
+      '/price/fullbody3.png',
+    ],
+  },
+  {
+    title: 'Panorama',
+    body: `You can add a 360° panorama to your commission`,
+    price: '+$40',
+    images: ['/price/panorama1.png'],
+  },
+  {
+    title: 'Extras',
+    body: `Alt versions $15 each /
+Panels are $20-40 each, depending on complexity /
+Private commissions + 50% of the total price /`,
+    price: 'Price varies',
+    images: ['/price/additional1.png', '/price/additional2.png'],
+  },
+]
+
 const Price = () => {
   return (
     <div className="flex-grow container max-w-screen-lg mx-auto p-4 bg-base-100">
-      <div className=" prose max-w-prose text-lg">
+      <div className="prose max-w-prose text-lg mb-8">
         <p>Thank you for taking interest in my commissions! </p>
 
         <p>
@@ -53,18 +154,11 @@ const Price = () => {
             [Terms of Service]
           </a>
         </p>
-
-        <p>
-          Here's a link to my{' '}
-          <a
-            href="https://www.furaffinity.net/commissions/lapushis/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            [current price sheet]
-          </a>
-        </p>
       </div>
+      <div className="divider uppercase text-2xl">Commission types</div>
+      {COMMISSION_INFO.map((info, index) => (
+        <CommissionTypeCard {...info} cardPosition={index} />
+      ))}
     </div>
   )
 }
